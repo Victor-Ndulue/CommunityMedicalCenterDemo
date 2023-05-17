0410e90f12cef7262c62c21f8e95fbaaa7723753
Victor <ndulue.victor.c@gmail.com>
2023-05-17 23:46:32 +0100

    Updates added to the Persistence layer

--- CMCDemo.Persistence/Common/RepositoryContext.cs
+++ CMCDemo.Persistence/Common/RepositoryContext.cs
@@ -5,12 +5,16 @@ namespace CMCDemo.Persistence.Common
 {
     public class RepositoryContext:DbContext
     {
-        public RepositoryContext(DbContextOptions<RepositoryContext> options):base(options)
+        public RepositoryContext(DbContextOptions options):base(options)
         { 
         
         }
 
         public DbSet<Department> Departments { get; set; }
         public DbSet<Community_Medical_Centers> Community_Medical_Centers { get; set; }
+        public DbSet<MedicalCenterAddress> MedicalCenterAddress { get;set; }
+        public DbSet<ProfessionalStaff> ProfessionalStaff { get; set; }
+        public DbSet<Ref_Disciplines> Ref_Disciplines { get; set; }
+        public DbSet<StaffAddress> StaffAddress { get; set;}
     }
 }
