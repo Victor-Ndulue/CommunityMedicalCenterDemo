43eb46f931cae636ff880cc0824c9c1918134a1f
Marvelous-iAcademy <130442240+Marvelous-iAcademy@users.noreply.github.com>
2023-05-20 11:59:08 +0100

    update

--- CMCDemo.Persistence/Common/RepositoryContext.cs
+++ CMCDemo.Persistence/Common/RepositoryContext.cs
@@ -9,6 +9,7 @@ namespace CMCDemo.Persistence.Common
         { 
         
         }
+        // linking models with the database
 
         public DbSet<Department> Departments { get; set; }
         public DbSet<Community_Medical_Centers> Community_Medical_Centers { get; set; }
@@ -16,5 +17,14 @@ namespace CMCDemo.Persistence.Common
         public DbSet<ProfessionalStaff> ProfessionalStaff { get; set; }
         public DbSet<Ref_Disciplines> Ref_Disciplines { get; set; }
         public DbSet<StaffAddress> StaffAddress { get; set;}
+        public DbSet<AppointmentCategory> AppointmentCategories { get; set; }
+        public DbSet<Appointments> Appointments { get; set; }
+        public DbSet<AuditableBaseEntity> AuditableBaseEntities { get; set; }
+        public DbSet<CommonAddress> CommonAddress { get; set; }
+        public DbSet<Patients> Patients { get; set; }
+        public DbSet<Ref_Appt_Locations> Ref_Appt_Locations { get; set; }
+        public DbSet<Staff_in_Appointments> Staff_in_Appointments { get; set; }
+
     }
 }
+
